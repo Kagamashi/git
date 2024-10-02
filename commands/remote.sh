579a41cd2f0e90002966c56f1a30716f38b10c6f
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-10-02 19:12:29 +0200

    Modified: commands/remote.sh

--- commands/remote.sh
+++ commands/remote.sh
@@ -2,11 +2,11 @@ A remote repository is a version of your project that is hosted on a remote serv
 git remote allows you to manage connections to these remote repositories.
 
 # add remote repository
-git remote add <remote_name> <repository_url>
+git remote add [remote_name] [repository_url]
 git remote add origin https://github.com/user/repo.git # origin is the default name given to the remote repository
 
 # list remote repositories
 git remote -v
 
 # remove remote repository
-git remote remove <remote_name>
+git remote remove [remote_name]
