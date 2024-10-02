d578eb0d05264c5b1080e564cd19449201449a5a
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-10-02 19:12:28 +0200

    Modified: commands/push.sh

--- commands/push.sh
+++ commands/push.sh
@@ -2,10 +2,13 @@ git push is used to send your local commits to a remote repository.
 This command updates the remote repository with your new commits.
 
 # push changes to default remote (origin) and branch
-git push origin <branch_name>
+git push origin [branch_name]
 
 # push all branches to the remote
 git push --all
 
 # push tags to the remote
 git push --tags
+
+# remove remote branch
+git push origin --delete [branch_name]
