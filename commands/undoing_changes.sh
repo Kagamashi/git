eaa79aae78311a131ec75405b734775986002847
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-10-02 19:12:29 +0200

    Modified: commands/undoing_changes.sh

--- commands/undoing_changes.sh
+++ commands/undoing_changes.sh
@@ -1,5 +1,5 @@
 git reset: Moves the HEAD pointer and optionally discards or keeps changes.
-git checkout -- <file_name>: Discards uncommitted changes in a specific file.
+git checkout -- [file_name]: Discards uncommitted changes in a specific file.
 git revert: Safely undoes a commit by creating a new commit that reverses the changes.
 
 ---
@@ -8,13 +8,13 @@ git reset is used to undo changes to our working directory and move HEAD pointer
 This can be destructive (might remove our changes)
 
 # SOFT RESET : moves HEAD to a previous commit, but keeps your changes in the working directory and staging area
-git reset --soft <commit>
+git reset --soft [commit]
 
 # MIXED RESET (default) : moves HEAD to a previous commit and keeps changes in the working directory but unstages them
-git reset --mixed <commit>
+git reset --mixed [commit]
 
 # HARD RESET : moves HEAD to previous commit and discards all changes
-git reset --hard <commit>
+git reset --hard [commit]
 
 ---
 
@@ -22,7 +22,7 @@ git checkout -- is used to discard changes in the working directory that have no
 (not added to staging area)
 
 # discard changes to specific file
-git checkout -- <file_name>
+git checkout -- [file_name]
 
 ---
 
@@ -30,10 +30,10 @@ git revert is safer way to undo changes by creating a new commit that undoes the
 This preserves the project history 
 
 # revert specific commit
-git revert <commit>
-# this creates a new commiit that undoes the changes made by <commit> while leaving other commits intact
+git revert [commit]
+# this creates a new commiit that undoes the changes made by [commit] while leaving other commits intact
 
 --- 
 
 git restore is used to discard changes in working directory
-does the same thing as 'git checkout -- <file_name>'
+does the same thing as 'git checkout -- [file_name]'
