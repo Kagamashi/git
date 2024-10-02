785c5c459dd41a158bc2f27aec45ec5356628753
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-10-02 19:12:28 +0200

    Modified: commands/merge.sh

--- commands/merge.sh
+++ commands/merge.sh
@@ -2,7 +2,7 @@ git merge is used to combine the changes from one branch into another. Typically
 
 # merge branch into current branch
 # this command will integrate changes from branch_name into branch we are currently on
-git merge <branch_name>
+git merge [branch_name]
 
 # Fast-forward merge: If there are no new commits in the target branch, Git will move the pointer forward (no extra commit is needed).
 # Three-way merge: If both branches have commits, Git will create a new merge commit combining both histories.
