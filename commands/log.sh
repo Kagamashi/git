62ea7cd4eb2358085784defc91058ff940dbe320
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-10-02 19:12:28 +0200

    Modified: commands/log.sh

--- commands/log.sh
+++ commands/log.sh
@@ -12,6 +12,9 @@ git log --graph --oneline --all
 # limit number of commits displayed
 git log -3
 
----
+# sHOW NUMBER OF COMMITS DONE BY AUTHORS
+git shortlog -s -n --all --no-merges --since="22 Aug 2019" --before="01 Sept 2019" 
+git shortlog --summary --numbered --all --no-merges
 
+# Lists all commits - even the lost (deleted/moved) ones
 git log --graph --decorate --oneline $(git rev-list -g --all
