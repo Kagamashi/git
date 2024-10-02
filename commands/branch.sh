b1d44f1de8b0fae04be9a58f7be092146040fcfd
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-10-02 19:12:28 +0200

    Modified: commands/branch.sh

--- commands/branch.sh
+++ commands/branch.sh
@@ -2,14 +2,14 @@ Branches allow you to work on different features, fixes, or experiments in isola
 git branch creates a new branch that starts from the current commit.
 
 # create new branch
-git branch <branch_name>
+git branch [branch_name]
 
 # list all branches 
 git branch # currently active branch will be marked with asterisk (*)
 
 # delete a branch (once no longer needed)
-git branch -d <branch_name>
+git branch -d [branch_name]
 
 # force delete (if branch is not fully merged)
-git branch -D <branch_name>
+git branch -D [branch_name]
 
