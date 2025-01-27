a3da71daf81493e6acac10ed33f65bc57cc9bc75
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-01-27 22:41:35 +0100

    Modified: commands/commit.sh

--- commands/commit.sh
+++ commands/commit.sh
@@ -1,5 +1,5 @@
-git commit takes the changes from the staging area and stores them in the repository's history.
-Every commit is associated with a unique ID (SHA-1 hash) and includes a message that describes the changes.
+# git commit takes the changes from the staging area and stores them in the repository's history.
+# Every commit is associated with a unique ID (SHA-1 hash) and includes a message that describes the changes.
 
 # commit stages changes with message
 git commit -m "Your commit message"
