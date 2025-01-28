011a793410920278667d85b1589ea8cf40f64621
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-01-28 10:14:42 +0100

    Modified: commands/branch.sh

--- commands/branch.sh
+++ commands/branch.sh
@@ -1,5 +1,4 @@
-Branches allow you to work on different features, fixes, or experiments in isolation from the main codebase.
-git branch creates a new branch that starts from the current commit.
+# Branches allows to work on different features, fixes, or experiments in isolation from the main codebase.
 
 # create new branch
 git branch [branch_name]
@@ -7,9 +6,8 @@ git branch [branch_name]
 # list all branches 
 git branch # currently active branch will be marked with asterisk (*)
 
-# delete a branch (once no longer needed)
+# delete a branch
 git branch -d [branch_name]
 
 # force delete (if branch is not fully merged)
 git branch -D [branch_name]
-
