75b8ea9995ed5082c3145243f74d8905099c41c4
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-01-28 09:56:42 +0100

    Modified: commands/remote.sh

--- commands/remote.sh
+++ commands/remote.sh
@@ -1,5 +1,5 @@
-A remote repository is a version of your project that is hosted on a remote server (e.g., GitHub, GitLab, Bitbucket).
-git remote allows you to manage connections to these remote repositories.
+# A remote repository is a version of your project that is hosted on a remote server (e.g., GitHub, GitLab, Bitbucket).
+# git remote allows you to manage connections to these remote repositories.
 
 # add remote repository
 git remote add [remote_name] [repository_url]
