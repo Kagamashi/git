2dfd6110e702507a31908ea0502184dd2b2ca861
Mikolaj <mikolaj.borecki1@gmail.com>
2025-02-15 23:40:07 +0100

    Added new file: 02_core_commands/

---
+++ 02_core_commands/commiting_changes/git_commit.sh
@@ -0,0 +1,12 @@
+# git commit
+# Takes the changes from the staging area and stores them in the repository's history.
+# Every commit is associated with a unique ID (SHA-1 hash) and includes a message that describes the changes.
+
+# commit stages changes with message
+git commit -m "Your commit message"
+
+# stage and commit all changes at once (skipping git add)
+git commit -a -m "Your commit message"
+
+# amend last commit (wthout creating a new one)
+git commit --amend
